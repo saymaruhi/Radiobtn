df46a00ea73616102076cbd8bbb9c2f09b94be07
sayma ruhi <saymaruhi@outlook.com>
2025-05-05 16:12:40 -0400

    added student function

--- src/user.jsx
+++ src/user.jsx
@@ -1,8 +1,40 @@
-function User(){
-    return(
-        <div>
-            <h3>Component Loop</h3>
-        </div>
-    )
+function User() {
+  const student = [
+    {
+      Name: "Sayma",
+      School: "A.N College",
+      Degree: "MBA",
+    },
+    {
+      Name: "Sajid",
+      School: "SMU",
+      Degree: "MCA",
+    },
+  ];
+
+  return (
+    <div>
+      <h3>Component Loop</h3>
+      <table border={1}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>School</td>
+            <td>Degree</td>
+          </tr>
+        </thead>
+        <tbody>
+          <td>Sayma</td>
+          <td>A.N.College</td>
+          <td>M.B.A</td>
+        </tbody>
+        <tbody>
+        <td>Sajid</td>
+          <td>SMU</td>
+          <td>M.C.A</td>
+        </tbody>
+      </table>
+    </div>
+  );
 }
-export default User
+export default User;
